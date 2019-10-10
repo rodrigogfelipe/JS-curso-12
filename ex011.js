02433ef763b1988eb88236eae5b1f5a40ddd8d1c
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-10 15:52:20 -0300

    Condições parte - 02

---
+++ ex011.js
@@ -0,0 +1,12 @@
+/*var idade= 16
+if(idade < 16) {
+    console.log('Menor idade')
+
+} else {
+    console.log('Maior idade')
+
+} */
+
+var agora= new Date()
+var hora= agora.getHours()
+console.log(hora)
---
+++ ex013.js
@@ -0,0 +1,41 @@
+var agora= new Date()
+//var diaSem= agora.getDay()
+
+/* 
+0-domingo
+1-segunda
+2-terça
+3-quarta
+4-quinta
+5-sexta
+6-sabado */
+
+
+diaSem= 10
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+
+    case 1:
+        console.log('Segunda')
+        break
+
+    case 2:
+        console.log('Terça')
+            break
+
+    case 3:
+        console.log('Quarta')
+            break
+
+    case 4:
+        console.log('Quinta')
+        break
+    default:
+        console.log('Codigo invalido')
+        break
+
+
+}
+
